eb360029e422fdbbc9dee67c987b8515f4b9446c
baliguan163 <baliguan163@163.com>
2018-10-25 10:24:42 +0800

    测试

---
+++ src/main/java/com/company/project/service/TianMaoGoodsService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.TianMaoGoods;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by ado on 2018-09-07.
+ */
+public interface TianMaoGoodsService extends Service<TianMaoGoods> {
+
+}
